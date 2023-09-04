6c289277a90ee335be9f1ee9a3086a5400acb037
Marat <email.marat@gmail.com>
2023-09-04 19:00:21 +0300

    added HW_5

--- HW_4/hwapp/admin.py
+++ HW_4/hwapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Customer, Product, Order
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'price']
+    ordering = ['name']
+    list_filter = ['date_add', 'price']
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone', 'address']
+    ordering = ['name']
+
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order)
